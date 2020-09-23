5c5a013ace95e0e7cc21c556afba214575eb4bee
mmontagne <marcelomontagne@live.com>
2020-09-23 18:22:12 -0300

    Change title

--- src/components/PxHeader.vue
+++ src/components/PxHeader.vue
@@ -7,7 +7,7 @@
           <router-link
             :to="{ name: 'home' }"
             class="font-semibold text-xl tracking-tight"
-            >PlatziExchange</router-link
+            >CryptoRanking</router-link
           >
         </div>
         <div
